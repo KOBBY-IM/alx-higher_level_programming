e301c240ab37e512d16f18dbb19ede888a6e5c60
KOBBY-IM <nyarkotheo2@gmail.com>
2023-05-11 21:21:12 +0000

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     import hidden_4
 
     names = dir(hidden_4)
-    for name in sorted(names):
-        if name[0] != "__":
+    for name in names:
+        if name[:2] != "__":
             print(name)
