d33b4074c40fc13f08f9092e6bede1ddcbbaaffd
KOBBY-IM <nyarkotheo2@gmail.com>
2023-05-15 23:31:09 +0000

    task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,17 +1,10 @@
-#!/usr/bin/python3
+m #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
     # Check for empty list.
     if not my_list:
         return []
 
-    # Check for invalid index.
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-
-    # Copy the list to a new list, without the item at
-    # the specified index.
-    new_list = my_list[:idx] + my_list[idx + 1:]
-
-    # Return the new list.
-    return new_list
+    if idx < 0 and idx >= len(my_list):
+        del my_list[idx]
+    return my_list
