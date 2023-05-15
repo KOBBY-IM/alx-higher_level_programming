75240fd4c29c6e79edebebdbec10a9ed1f1e28c0
KOBBY-IM <nyarkotheo2@gmail.com>
2023-05-15 21:19:19 +0000

    Task 6, changed print and added a not statement

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
     for row in matrix:
         for element in row:
-            print("{0}".format(element), end=' ')
+            print("{}".format(element), end=" ")
         print()
