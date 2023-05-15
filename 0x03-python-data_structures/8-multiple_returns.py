6656819726ad5021981107ab26843ba56c1492ac
KOBBY-IM <nyarkotheo2@gmail.com>
2023-05-15 22:57:23 +0000

    pycodestyle error

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-  # Check for empty string.
-  if not sentence:
-    return (0, None)
+    # Check for empty string.
+    if not sentence:
+        return (0, None)
 
-  # Get the length of the string.
-  length = len(sentence)
+    # Get the length of the string.
+    length = len(sentence)
 
-  # Get the first character of the string.
-  first_character = sentence[0]
+    # Get the first character of the string.
+    first_character = sentence[0]
 
-  return (length, first_character)
+    return (length, first_character)
