ef067c4a9165377865664bae0fe3a8507711269f
KOBBY-IM <nyarkotheo2@gmail.com>
2023-05-17 21:51:23 +0000

    Task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    # Get the length of the dictionary.
+    length = len(a_dictionary)
+
+    # Return the length of the dictionary.
+    return length
