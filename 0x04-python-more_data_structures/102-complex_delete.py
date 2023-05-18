3600c3a24af5b4f110aa859a44779526b8a9e1af
KOBBY-IM <nyarkotheo2@gmail.com>
2023-05-18 00:23:32 +0000

    Task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
-    if value not in a_dictionary:
-        return a_dictionary
+    #if value not in a_dictionary:
+        #return a_dictionary
 
     # Create a new dictionary without the keys that have the searched value.
-    new_dictionary = {}
-    for key, value in a_dictionary.items():
-        if value != value:
-            new_dictionary[key] = value
-
-    return new_dictionary
+    for key in list(a_dictionary.keys()):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
+    return(a_dictionary)
