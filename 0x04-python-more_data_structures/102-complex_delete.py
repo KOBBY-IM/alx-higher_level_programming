9b4dfcb6af8d158fd63b8a61a1ac1746c2437f7f
KOBBY-IM <nyarkotheo2@gmail.com>
2023-05-18 00:28:06 +0000

    fixed codestyle error

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
-    #if value not in a_dictionary:
-        #return a_dictionary
+    # if value not in a_dictionary:
+    # return a_dictionary
 
-    # Create a new dictionary without the keys that have the searched value.
+    # Create a new dictionary without the keys that have
+    # the searched value.
     for key in list(a_dictionary.keys()):
         if a_dictionary[key] == value:
             del a_dictionary[key]
+
     return(a_dictionary)
