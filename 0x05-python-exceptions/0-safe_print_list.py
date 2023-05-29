86b95ceba26cac767a96a623215e6fd38a603835
KOBBY-IM <nyarkotheo2@gmail.com>
2023-05-29 20:25:56 +0000

    Task 0, pycodestyle error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -12,6 +12,7 @@ def safe_print_list(my_list=[], x=0):
     return i
 """
 
+
 def safe_print_list(my_list=[], x=0):
     i = 0
     while i < x:
@@ -21,4 +22,4 @@ def safe_print_list(my_list=[], x=0):
             break
         i += 1
     print("")
-    return i
+    return igit a
