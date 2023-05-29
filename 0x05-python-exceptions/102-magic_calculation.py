762528a56c441bc83e9e92b45ff81702fd19fc60
KOBBY-IM <nyarkotheo2@gmail.com>
2023-05-29 22:44:22 +0000

    Task 9, used exception clause to catch all exceptions

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,6 +7,8 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception("Too far")
             else:
-                result += (a**b)/i
-
-    return result + b
+                result += (a**b) / i
+        except Exception:
+            result = b + a
+            break
+    return (result)
