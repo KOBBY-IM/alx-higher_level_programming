5b79cc7400b6af7706105dcafae2e4c13e5186cc
KOBBY-IM <nyarkotheo2@gmail.com>
2023-08-31 17:09:27 -0700

    created executable for all task and tested -o /dev/null option and the -w to display message in task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # This script makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -s "0.0.0.0:5000/catch_me" | grep -o "You got me!"
+
+curl -sX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -L -o /dev/null -w "You got me!"
